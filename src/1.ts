94d022942a999e02bef7b8bc0bfb7756d3639c2c
Daryna Holikova <dgolikova0@gmail.com>
2023-11-16 21:50:19 +0000

    done

--- src/1.ts
+++ src/1.ts
@@ -4,16 +4,18 @@
   напишіть скорочену ініціалізацію.
 */
 
-class Student {
-  public name: string;
-  public age: number;
-  public grade: string;
-
-  constructor(name: string, age: number, grade: string) {
-    this.name = name;
+class Studentt{
+  constructor(public name: string, public age: number, public grade: string) {
+        this.name = name;
     this.age = age;
     this.grade = grade;
   }
 }
 
+
+
 export {};
+
+
+
+
